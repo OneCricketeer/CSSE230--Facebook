75ec8ba5d15c23b3790b4094297684368de37766
moorejm <moorejm@rose-hulman.edu>
2013-02-05 16:38:49 -0500

    Classes

---
+++ CODE/src/SixDegrees.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+
+public class SixDegrees {
+
+	static HashMap<Integer, User> users;
+	static HashMap<String, Group> groups;
+	User current;
+	
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CODE/src/User.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class User {
+	private static int userCount;
+	private Integer uid;
+	private String name, status, userName;
+	private Calendar calendar;
+	private String work;
+	private String dorm;
+	private ArrayList<String> organizations;
+	private TreeSet<User> friends;
+	
+	public User() {
+		
+	}
+	
+	public User(int id, String userName, String name) {
+		id = userCount++;
+		userName = userName;
+		name = name;
+	}
+	
+	public void addFriend(User friend) {
+		this.friends.add(friend);
+	}
+	
+	public User[] getFriends() {
+		 return this.friends.toArray(new User[0]);
+	}
+	
+	
+}
